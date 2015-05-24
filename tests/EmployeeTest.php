a97c9677e4cf079cb812c083177e4dc18d5e1e49
Per Olofsson <pelle@pqz.se>
2015-05-24 17:30:45 +0200

    refactor EmployeeTest

--- tests/EmployeeTest.php
+++ tests/EmployeeTest.php
@@ -5,13 +5,11 @@ use DsvSu\Daisy\Employee;
 
 class EmployeeTest extends TestCase
 {
-    const MOCK_EMPLOYEE_1 = '{"person":{"id":1234,"firstName":"Fil","lastName":"Ur","email":"fil-ur@dsv.su.se","lastChanged":1418743460013},"departments":[{}],"office":"10:9","workPhone":"161625","title":"Troll","title_en":"Ogre"}';
-    
     public function testGetById()
     {
-        $this->mockData(self::MOCK_EMPLOYEE_1);
-
+        $this->mockData('{"person":{"id":1234,"firstName":"Fil","lastName":"Ur","email":"fil-ur@dsv.su.se","lastChanged":1418743460013},"departments":[{}],"office":"10:9","workPhone":"161625","title":"Troll","title_en":"Ogre"}');
         $e = Employee::findByUsername('fil-ur');
+
         $this->assertInstanceOf('DsvSu\Daisy\Employee', $e);
 
         $req = $this->getRequest();
@@ -22,11 +20,55 @@ class EmployeeTest extends TestCase
         );
         $this->assertCount(0, $req->getQuery());
 
+        return $e;
+    }
+
+    /**
+     * @depends testGetById
+     */
+    public function testGetOffice(Employee $e)
+    {
         $this->assertEquals('10:9', $e->getOffice());
+    }
+
+    /**
+     * @depends testGetById
+     */
+    public function testGetTitle(Employee $e)
+    {
         $this->assertEquals('Troll', $e->getTitle());
         $this->assertEquals('Ogre', $e->getTitle('en'));
-        $this->assertEquals('161625', $e->getWorkPhone());
+    }
 
+    /**
+     * @depends testGetById
+     */
+    public function testGetWorkPhone(Employee $e)
+    {
+        $this->assertEquals('161625', $e->getWorkPhone());
+    }
+
+    /**
+     * @depends testGetById
+     */
+    public function testGetCompleteWorkPhone(Employee $e)
+    {
+        $this->assertEquals('+468161625', $e->getCompleteWorkPhone());
+    }
+
+    /**
+     * @depends testGetById
+     */
+    public function testGetWorkPhoneExtension(Employee $e)
+    {
+        $this->assertEquals('1625', $e->getWorkPhoneExtension());
+    }
+
+    /**
+     * @depends testGetById
+     */
+    public function testGetPerson(Employee $e)
+    {
         $p = $e->getPerson();
         $this->assertInstanceOf('DsvSu\Daisy\Person', $p);
         $this->assertEquals(1234, $p->getId());
