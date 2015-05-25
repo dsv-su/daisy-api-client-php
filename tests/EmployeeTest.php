8306b6a0c478ae4056af9defa7dd43e651b88fb3
Per Olofsson <pelle@dsv.su.se>
2015-05-25 14:13:11 +0200

    fix workphone test

--- tests/EmployeeTest.php
+++ tests/EmployeeTest.php
@@ -5,7 +5,7 @@ use DsvSu\Daisy\Employee;
 
 class EmployeeTest extends TestCase
 {
-    public function testGetById()
+    public function testFindByUsername()
     {
         $this->mockData('{"person":{"id":1234,"firstName":"Fil","lastName":"Ur","email":"fil-ur@dsv.su.se","lastChanged":1418743460013},"departments":[{}],"office":"10:9","workPhone":"161625","title":"Troll","title_en":"Ogre"}');
         $e = Employee::findByUsername('fil-ur');
@@ -24,7 +24,7 @@ class EmployeeTest extends TestCase
     }
 
     /**
-     * @depends testGetById
+     * @depends testFindByUsername
      */
     public function testGetOffice(Employee $e)
     {
@@ -32,7 +32,7 @@ class EmployeeTest extends TestCase
     }
 
     /**
-     * @depends testGetById
+     * @depends testFindByUsername
      */
     public function testGetTitle(Employee $e)
     {
@@ -41,15 +41,20 @@ class EmployeeTest extends TestCase
     }
 
     /**
-     * @depends testGetById
+     * @depends testFindByUsername
      */
     public function testGetWorkPhone(Employee $e)
     {
-        $this->assertEquals('161625', $e->getWorkPhone());
+        $p = $e->getWorkPhone();
+        $this->assertInstanceOf('\libphonenumber\PhoneNumber', $p);
+        $this->assertEquals(46, $p->getCountryCode());
+        $this->assertEquals('161625', $p->getNationalNumber());
+        $this->assertEquals('+468161625', strval($p));
+        $this->assertEquals('08161650', $p->getNationalNumber());
     }
 
     /**
-     * @depends testGetById
+     * @depends testFindByUsername
      */
     public function testGetPerson(Employee $e)
     {
