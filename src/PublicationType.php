dace26521ce040fd665f1ee917737d3045626e4a
Per Olofsson <pelle@dsv.su.se>
2015-09-01 16:55:25 +0200

    start implementing PublicationType

--- src/PublicationType.php
+++ src/PublicationType.php
@@ -1,7 +1,74 @@
 <?php
 namespace DsvSu\Daisy;
 
-class PublicationType extends SplEnum
+class PublicationType
 {
-    const PAPER = 1;
+    /** @var PublicationType[] */
+    private static $registry = [];
+
+    /** @var string */
+    private $identifier;
+
+    /** @var string */
+    private $svName;
+
+    /** @var string */
+    private $enName;
+
+    public function __initialize($identifier, $svName, $enName)
+    {
+        $this->identifier = $identifier;
+        $this->svName = $svName;
+        $this->enName = $enName;
+        self::$registry[$identifier] = $this;
+    }
+
+    public function getIdentifier()
+    {
+        return $this->identifier;
+    }
+
+    public function getName($lang = 'sv')
+    {
+        if ($lang === 'sv') {
+            return $this->svName;
+        } else if ($lang === 'en') {
+            return $this->enName;
+        } else {
+            
+        }
+    }
+
+    private static function init()
+    {
+        new PublicationType('comprehensiveDoctoralThesis');
+        new PublicationType('comprehensiveLicentiateThesis');
+        new PublicationType('studentThesis');
+        new PublicationType('article');
+        new PublicationType('survey');
+        new PublicationType('review');
+        new PublicationType('book');
+        new PublicationType('chapter');
+        new PublicationType('collect');
+        new PublicationType('conference');
+        new PublicationType('paper');
+        new PublicationType('keynote');
+        new PublicationType('manuscript');
+        new PublicationType('board');
+        new PublicationType('opponent');
+        new PublicationType('patent');
+        new PublicationType('report');
+        new PublicationType('other');
+    }
+
+    public static function getAll()
+    {
+        init();
+    }
+
+    public static function getByIdentifier($identifier)
+    {
+        init();
+        return self::$registry[$identifier];
+    }
 }
