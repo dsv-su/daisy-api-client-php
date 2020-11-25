89f5a93166e06a4917e92f1c58cea5805a20c6b2
Erik Thuning <boooink@gmail.com>
2020-11-25 16:28:47 +0100

    Updated the research area list

--- src/ResearchArea.php
+++ src/ResearchArea.php
@@ -35,29 +35,33 @@ class ResearchArea
     {
         if (empty(self::$registry)) {
             new self(10, "Business Process Management and Enterprise Modeling");
-            new self(11, "Consumer-oriented mobile services");
+            new self(26, "Cyber Security");
             new self(12, "Data Science");
-            new self(13, "Digital games");
-            new self(14, "Digital Systems Security");
+            new self(27, "Digital Games and Simulation");
+            new self(28, "Digital Innovation");
             new self(15, "E-government and E-democracy");
-            new self(17, "Healthcare Informatics");
+            new self(17, "Health Informatics");
             new self(16, "ICT for Development");
             new self(24, "Immersive Networking");
             new self(18, "Interaction design");
-            new self(19, "IT Management");
+            new self(25, "IT Management and Governance");
             new self(20, "Language Technology");
             new self(21, "Risk and decision analysis");
-            new self(22, "Service Science & Innovation");
+            new self(30, "Software Science");
             new self(23, "Technology Enhanced Learning");
-            new self(9, "(Datorspel)");
-            new self(2, "(Informationssystem)");
-            new self(1, "(Informationssäkerhet)");
-            new self(7, "(IT för hälsa)");
-            new self(8, "(IT för lärande)");
-            new self(3, "(Kunskap och kommunikation)");
+            new self(9, "(Computer Games)");
+            new self(11, "(Consumer-oriented mobile services)");
+            new self(14, "(Digital Systems Security)");
+            new self(1, "(Information security)");
+            new self(2, "(Information system)");
+            new self(7, "(IT for health)");
+            new self(8, "(IT for learning)");
+            new self(19, "(IT Management)");
+            new self(3, "(Knowledge and communication)");
             new self(4, "(Mobile Life)");
-            new self(5, "(Programvaruutveckling)");
-            new self(6, "(Systemanalys)");
+            new self(22, "(Service Science & Innovation)");
+            new self(5, "(Software development)");
+            new self(6, "(System analysis)");
         }
         return self::$registry;
     }
